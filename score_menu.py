ef8178dd0a816ca6402899c34e036b38627f4b04
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-01 16:51:33 +0800

    All finished with optionS, print "*" just need one line, so i don't make it a function.

--- score_menu.py
+++ score_menu.py
@@ -1,19 +1,15 @@
-# score_menu_v3.py
-
 def main():
-
-
+    '''Display menu , get option'''
     MENU = "\n(G)et valid score\n(P)rint result\n(S)how stars\n(Q)uit"
     print(MENU)
-    print('what is your choice')
     choice = input(">>> ").upper()
-
     while choice != "Q":
         if choice == "G":
             score = get_valid_score()
         elif choice == "P":
             print(get_score_result(score))
-
+        elif choice == "S":
+            print("*" * int(score))
         else:
             print("Invalid option")
         print(MENU)
@@ -23,14 +19,14 @@ def main():
 
 
 def get_valid_score():
+    '''det score and check'''
     score = float(input("Enter score (0-100): "))
     while score < 0 or score > 100:
         print("Invalid score. Must be between 0 and 100.")
         score = float(input("Enter score (0-100): "))
     return score
-
-
 def get_score_result(score):
+    '''Rank the score'''
     if score > 90:
         return "Excellent"
     elif score >= 50:
