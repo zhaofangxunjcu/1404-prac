4c59d0c544d0cdc58d7c7ae30d00fde9c387caca
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-01 15:59:06 +0800

    original file

---
+++ score.py
@@ -0,0 +1,31 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+# TODO: Fix this!
+score = float(input("Enter score: "))
+if score < 0 or score > 100:
+    print("Invalid score")
+elif score > 90:
+    print("Excellent")
+elif score > 50:
+    print("Passable")
+else:
+    print("Bad")
+
+#
+# score = float(input("Enter score: "))
+# if score < 0:
+#     print("Invalid score")
+# else:
+#     if score > 100:
+#         print("Invalid score")
+#     if score > 50:
+#         print("Passable")
+#     #reprat
+#     if score > 90:
+#     print("Excellent")
+#     #Indentation Error
+# if score < 50:
+#     print("Bad")
