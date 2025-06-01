f95ac19fafe90f4c339ab8ebc49676bb44f88bce
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-01 15:27:13 +0800

    task2,use refactor extract methods to make function

--- password_stars.py
+++ password_stars.py
@@ -1,13 +1,18 @@
 def main():
     MIN_LENGTH = 6
 
-    password = input("Enter a password (at least {} characters): ".format(MIN_LENGTH))
+    password = get_password(MIN_LENGTH)
 
+    print("*" * len(password))
+
+
+def get_password(MIN_LENGTH):
+    password = input("Enter a password (at least {} characters): ".format(MIN_LENGTH))
     while len(password) < MIN_LENGTH:
         print("Password is too short. Please try again.")
         password = input("Enter a password (at least {} characters): ".format(MIN_LENGTH))
+    return password
 
-    print("*" * len(password))
 
 main()
 
