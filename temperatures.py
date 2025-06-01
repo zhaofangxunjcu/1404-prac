4e4197a5f76240870abcb3a7b5406af98f7a9beb
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-01 15:49:59 +0800

    Refactor new function transfer celsius to fahrenheit

--- temperatures.py
+++ temperatures.py
@@ -1,4 +1,5 @@
 def main():
+    '''main '''
     MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
@@ -6,9 +7,7 @@ Q - Quit"""
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
-            print(f"Result: {fahrenheit:.2f} F")
+            celsius_to_fahrenheit()
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
             celsius = 5 / 9 * (fahrenheit - 32)
@@ -18,5 +17,13 @@ Q - Quit"""
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you.")
+
+
+def celsius_to_fahrenheit():
+    '''transfer celsius to fahrenheit'''
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print(f"Result: {fahrenheit:.2f} F")
+
 
 main()
