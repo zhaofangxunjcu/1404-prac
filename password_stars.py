82116dc5c42b9668b5141d990c4bb6b307e8d8b4
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-01 15:22:34 +0800

    task1,i put everything in main()

--- password_stars.py
+++ password_stars.py
@@ -1,9 +1,13 @@
-MIN_LENGTH = 6
+def main():
+    MIN_LENGTH = 6
 
-password = input("Enter a password (at least {} characters): ".format(MIN_LENGTH))
-
-while len(password) < MIN_LENGTH:
-    print("Password is too short. Please try again.")
     password = input("Enter a password (at least {} characters): ".format(MIN_LENGTH))
 
-print("*" * len(password))
+    while len(password) < MIN_LENGTH:
+        print("Password is too short. Please try again.")
+        password = input("Enter a password (at least {} characters): ".format(MIN_LENGTH))
+
+    print("*" * len(password))
+
+main()
+
