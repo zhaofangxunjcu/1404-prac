a52ee35062e18ba8c2af124bff7d8f9bb9b1c39d
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-08 21:05:45 +0800

    Write into file

--- capitalist_conrad.py
+++ capitalist_conrad.py
@@ -14,10 +14,11 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
-
+FILENAME = "stock.txt"
+out_file = open(FILENAME, 'w')
 price = INITIAL_PRICE
 number_of_days = 0
-print(f"${price:,.2f}")
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     number_of_days += 1
@@ -34,4 +35,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
