d06e7a1e593f7b070990247cfba51d1989aaef92
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-09 17:29:41 +0800

    task2

--- files.py
+++ files.py
@@ -1,7 +1,12 @@
-name = input("Enter your name: ")
-
-file = open("name.txt", "w")
-file.write(name)
+# name = input("Enter your name: ")
+#
+# file = open("name.txt", "w")
+# file.write(name)
+# file.close()
+#
+# print("Name has been written to name.txt.")
+file = open("name.txt", "r")
+name = file.read()
 file.close()
 
-print("Name has been written to name.txt.")
+print(f"Hi {name}!")
