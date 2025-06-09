fc488114396cbee4101d8651dbd796d51b4f39b3
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-09 17:39:10 +0800

    task3

--- files.py
+++ files.py
@@ -3,10 +3,13 @@
 # file = open("name.txt", "w")
 # file.write(name)
 # file.close()
-#
 # print("Name has been written to name.txt.")
-file = open("name.txt", "r")
-name = file.read()
-file.close()
-
-print(f"Hi {name}!")
+# file = open("name.txt", "r")
+# name = file.read()
+# file.close()
+# print(f"Hi {name}!")
+with open("numbers.txt", "r") as file:
+    first_number = int(file.readline())
+    second_number = int(file.readline())
+result = first_number + second_number
+print(result)
