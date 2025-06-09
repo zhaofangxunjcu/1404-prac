bdaeb14155cf8841e03ea6f0cd7f8bcbd7959e6c
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-09 17:42:03 +0800

    task4

--- files.py
+++ files.py
@@ -8,8 +8,14 @@
 # name = file.read()
 # file.close()
 # print(f"Hi {name}!")
+# with open("numbers.txt", "r") as file:
+#     first_number = int(file.readline())
+#     second_number = int(file.readline())
+# result = first_number + second_number
+# print(result)
 with open("numbers.txt", "r") as file:
-    first_number = int(file.readline())
-    second_number = int(file.readline())
-result = first_number + second_number
-print(result)
+    total = 0
+    for line in file:
+        total += int(line)
+print(total)
+
