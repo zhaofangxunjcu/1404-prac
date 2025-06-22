bda0073b50c82549948cdf803eecd330bed8df8d
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-22 15:17:29 +0800

    Change LBYL to EAFP by replacing key check with try-except block

--- prac05/state_names.py
+++ prac05/state_names.py
@@ -9,13 +9,21 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 # print(CODE_TO_NAME)
 #
-# state_code = input("Enter short state: ").upper()
-# while state_code != "":
-#     if state_code in CODE_TO_NAME:
-#         print(state_code, "is", CODE_TO_NAME[state_code])
-#     else:
-#         print("Invalid short state")
-#     state_code = input("Enter short state: ").upper()
-for code, name in CODE_TO_NAME.items():
-    print(f"{code:3} is {name}")
+# # state_code = input("Enter short state: ").upper()
+# # while state_code != "":
+# #     if state_code in CODE_TO_NAME:
+# #         print(state_code, "is", CODE_TO_NAME[state_code])
+# #     else:
+# #         print("Invalid short state")
+# #     state_code = input("Enter short state: ").upper()
+# for code, name in CODE_TO_NAME.items():
+#     print(f"{code:3} is {name}")
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    try:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    except KeyError:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
+
 
