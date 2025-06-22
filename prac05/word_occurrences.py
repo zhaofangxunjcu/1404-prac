f82df95d537aa93bf7d1dc2aef859f98a899a4b2
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-22 15:33:25 +0800

    Format word frequency output using dynamic width for alignment

--- prac05/word_occurrences.py
+++ prac05/word_occurrences.py
@@ -13,6 +13,7 @@ for word in words:
         word_count[word] += 1
     else:
         word_count[word] = 1
+max_word_length = max(len(word) for word in word_count)
 
 for word in sorted(word_count):
-    print(f"{word} : {word_count[word]}")
+    print(f"{word:{max_word_length}} : {word_count[word]}")
