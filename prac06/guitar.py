c43dfc3a4150f241e98d0df3ae2d7ddde87bf3eb
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-07-13 21:15:08 +0800

    Class guitar finished with docstring

--- prac06/guitar.py
+++ prac06/guitar.py
@@ -1,16 +1,20 @@
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar instance with name, year, and cost."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return a string representation of the guitar."""
         return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
 
     def get_age(self):
+        """Return the age of the guitar in years."""
         current_year = 2025  # Change this as needed
         return current_year - self.year
 
     def is_vintage(self):
+        """Return True if the guitar is 50 or more years old."""
         return self.get_age() >= 50
 
