7da82033977ee136f7466cebe57356298893e423
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-07-17 20:40:44 +0800

    project limo created

---
+++ prac06/used_cars.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 Practical - Client code to use the Car class.
+Note that the import has a folder (module) in it.
+This is why we name our folders with no spaces or capitals, as valid module names.
+"""
+
+from car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    limo = Car(100)
+    my_car = Car(180)
+    my_car.drive(30)
+    print(f"Car has fuel: {my_car.fuel}")
+    print(my_car)
+
+
+main()
