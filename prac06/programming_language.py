e0a7bb17b34f6767b901e492989a4b7c068cbcc6
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-07-17 19:09:16 +0800

    ADD str method to Class

--- prac06/programming_language.py
+++ prac06/programming_language.py
@@ -10,3 +10,7 @@ class ProgrammingLanguage:
 
     def is_dynamic(self):
         return self.type == 'Dynamic'
+
+    def __str__(self):
+        return (f"{self.name}, {self.type} Typing, Reflection={self.reflection},"
+                f" First appeared in {self.year}")
