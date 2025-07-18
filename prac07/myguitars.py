92f95d7c96a445adaed2d13837f5e97ed2094e7d
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-07-18 18:11:20 +0800

    Class and display by loop

---
+++ prac07/myguitars.py
@@ -0,0 +1,22 @@
+import csv
+from guitar import Guitar
+def main():
+    guitars = load_guitars("guitars.csv")
+    print("These are the guitars we have:")
+    for guitar in guitars:
+        print(guitar)
+def load_guitars(filename):
+    """read csv"""
+    guitars = []
+    with open(filename, newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for name, year, cost in reader:
+            guitars.append(Guitar(name.strip(), int(year), float(cost)))
+    return guitars
+
+
+
+
+
+if __name__ == "__main__":
+    main()
