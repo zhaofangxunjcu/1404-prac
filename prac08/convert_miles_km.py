23c507fd32363705d777aec26c52899948c05fe8
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-07-20 16:12:20 +0800

    Comment convert button in kv

--- prac08/convert_miles_km.py
+++ prac08/convert_miles_km.py
@@ -1,36 +1,45 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
-class Covertmileskm(App):
+
+# 1) Constant for conversion factor
+MILES_TO_KM = 1.60934
+
+class ConvertMilesKm(App):
     message = StringProperty()
+
     def build(self):
-        """Construct the app."""
         self.title = "Convert miles to km"
-        self.root = Builder.load_file('convert_miles_km.kv')
-        self.message = ""
-        return self.root
+        return Builder.load_file('convert_miles_km.kv')
+
     def convert(self):
+        """Read input, compute km, update label (0.0 on invalid)."""
         try:
             miles = float(self.root.ids.input_miles.text)
-            km = miles * 1.60934
+            km = miles * MILES_TO_KM
             self.root.ids.output_km.text = f'{km:.5f}'
         except ValueError:
             self.root.ids.output_km.text = '0.0'
 
     def increase(self):
+        """Add 1 to current miles (or start at 0), then convert."""
         try:
             value = float(self.root.ids.input_miles.text)
         except ValueError:
             value = 0
         value += 1
         self.root.ids.input_miles.text = str(int(value))
+        self.convert()
 
     def decrease(self):
+        """Subtract 1 from current miles (or start at 0), then convert."""
         try:
             value = float(self.root.ids.input_miles.text)
         except ValueError:
             value = 0
         value -= 1
         self.root.ids.input_miles.text = str(int(value))
+        self.convert()
 
-Covertmileskm().run()
+if __name__ == '__main__':
+    ConvertMilesKm().run()
