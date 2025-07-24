6546e0ef71e2eb3213f6e842bc30e56443ce9cdd
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-07-24 20:41:26 +0800

    simple code to test class

---
+++ prac-09/silver_service_taxi_test.py
@@ -0,0 +1,12 @@
+from silver_service_taxi import SilverServiceTaxi
+
+
+
+taxi = SilverServiceTaxi("Test Taxi", fuel=100, fanciness=2)
+taxi.start_fare()
+taxi.drive(18)
+fare = taxi.get_fare()
+print(taxi)
+print(f"Fare for 18 km: ${fare}")
+
+
