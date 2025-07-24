32be7b87efb59a215c81a60d1ea1b81b7bb423a8
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-07-24 19:13:31 +0800

    drive 100 and print current fare

--- prac-09/my_taxi.py
+++ prac-09/my_taxi.py
@@ -8,4 +8,9 @@ my_taxi.drive(40)
 print(my_taxi)
 print(f"Current fare: ${my_taxi.get_fare():.2f}")
 
+my_taxi.start_fare()
+my_taxi.drive(100)
 
+
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare():.2f}")
