ae98a3aea5aedf43eeef655ef1380788f8dc6fac
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-07-25 13:50:37 +0800

    use round make 48.78 to 48.80,try make 48.8 to 48.80

--- prac-09/silver_service_taxi.py
+++ prac-09/silver_service_taxi.py
@@ -11,8 +11,9 @@ class SilverServiceTaxi(Taxi):
         self.price_per_km = Taxi.price_per_km * fanciness
 
     def get_fare(self):
-        """Return full fare ."""
-        return round(super().get_fare() + self.flagfall, 2)
+        """Return the total fare, rounded to nearest 10c (includes flagfall)."""
+        total_fare = super().get_fare() + self.flagfall
+        return round(total_fare * 10) / 10
 
     def __str__(self):
         return f"{super().__str__()}, ${self.price_per_km:.2f}/km plus flagfall of ${self.flagfall:.2f}"
