2ac6d09023e9be83b6e17a29787d58625bbfef1a
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-07-26 21:39:25 +0800

    naming all the function that will be used

--- prac-09/taxi_simulator.py
+++ prac-09/taxi_simulator.py
@@ -1,2 +1,16 @@
+from pywin32_testutil import non_admin_error_codes
+
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
+def display_menu():
+    print("q)uit, c)hoose taxi, d)rive")
+
+def display_taxis(taxis):
+
+
+def choose_taxi(taxis):
+    non_admin_error_codes
+def drive_taxi(taxi):
+    try:
+        return 0
+
